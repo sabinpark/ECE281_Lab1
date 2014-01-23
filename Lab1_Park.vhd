6d3d0f2bfd0224ba524f83c01280962038b255e7
sabinpark <C16sabin.park@usafa.edu>
2014-01-22 23:14:35 -0700

    added the VHDL code

--- Lab1_Park.vhd
+++ Lab1_Park.vhd
@@ -1,18 +1,18 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company:  USAFA - ECE 281
+-- Engineer:  C3C Sabin Park 
 -- 
--- Create Date:    22:47:40 01/22/2014 
--- Design Name: 
--- Module Name:    Lab1_Park - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Create Date:  22:47:40 01/22/2014 
+-- Design Name:  Lab 1
+-- Module Name:  Lab1_Park - Behavioral 
+-- Project Name:  Lab 1
+-- Target Devices:  FGPA
+-- Tool versions:  1.0
+-- Description:  Takes in a 3-bit input and converts to the 2's complement
 --
--- Dependencies: 
+-- Dependencies:  N/A
 --
--- Revision: 
+-- Revision:  0.01
 -- Revision 0.01 - File Created
 -- Additional Comments: 
 --
@@ -39,9 +39,27 @@ entity Lab1_Park is
 end Lab1_Park;
 
 architecture Behavioral of Lab1_Park is
-
+	signal A_NOT, B_NOT, C_NOT, E, F, G, H, I : STD_LOGIC;
+	
 begin
-
+	-- the not's
+	A_NOT <= not A;
+	B_NOT <= not B;
+	C_NOT <= not C;
+	
+	-- x component
+	E <= A_NOT and C;
+	F <= A_NOT and B;
+	G <= A and B_NOT and C_NOT;
+	X <= E or F or G;
+	
+	-- y component
+	H <= B and C_NOT;
+	I <= B_NOT and C;
+	Y <= H or I;
+	
+	-- z component
+	Z <= C;
 
 end Behavioral;
 
